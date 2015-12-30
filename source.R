1b3e12542dee83611cfdd60f6c6921ef09e1bf4b
ColinRho <colinhwrho@gmail.com>
2015-12-30 14:39:02 +0900

    12.30 add data directory

--- source.R
+++ source.R
@@ -1,5 +1,6 @@
 ### TOPIC 1. 
 
+## 분석목적: 상품 출시 후 반응 진단 및 향후 매출 예측
 
 suppressPackageStartupMessages({
   
@@ -10,18 +11,16 @@ suppressPackageStartupMessages({
 })
 
 ###################################################################################################
-### Data import, manipulation
+### Data import, manipulation, detect outliers
 ###################################################################################################
 
-demo <- fread("Be the L.BA 주제1/1-1. Demo.txt")
-order <- fread("Be the L.BA 주제1/1-2. 구매내역정보.txt")
-
-order %>% select(ID) %>% unique()
-
+# two sets of data and merged data
+demo <- fread("TOPIC1/1-1. Demo.txt")
+order <- fread("TOPIC1//1-2. 구매내역정보.txt")
 merged <- merge(demo, order, by = "ID")
 
-# name of variables should be 'numeric'
-numeric_var <- c("ID", "성별", "연령", "구매시점", "구매시간", "구매요일", "구매건수", "구매금액",
+# variables should be treated as 'numeric'
+numeric_var <- c("성별", "연령", "구매시점", "구매시간", "구매요일", "구매건수", "구매금액",
   "취소여부", "평균기온")
 
 for ( i in 1:length(numeric_var) ) {
@@ -30,16 +29,64 @@ for ( i in 1:length(numeric_var) ) {
   
 }
 
+# to delete 'canceled' purchases  
+merged %>% select(취소여부) %>% table()
+merged %<>% filter(취소여부 != 1) %>% select(-취소여부)
 
-### 카테고리, 구매요일 제거
-
-
-
+# choosing the category
 merged %>% filter(카테고리 == "감자스낵") -> merged_potato
 
-merged_potato %>% group_by(구매시점, 상품구분) %>% 
-  summarise( n = n() ) %>% arrange(구매시점, 상품구분) %>% filter(상품구분 == "상품D") -> df
+merged %>% select(성별) %>% table
 
-ggplot(df, aes(x = 구매시점, y = n)) +
-  geom_point()
+merged %>% select(연령) %>% table
 
+merged %>% select(구매시간) %>% table
+
+###################################################################################################
+### Modularized functions
+###################################################################################################
+
+plotter <- 
+  
+  # drawing time vs number of purchaser given categories
+  # product
+  
+  function(df, product = "all", gender = "all", location = "all") {
+    
+    # filtering by inputs and counting a number of purchase(regardless of volume of purchase)
+    # or the volume should be considered? 
+    # depends on how to define the diffusion model. 
+    
+    if (product != "all") { 
+      
+      df %<>% filter(상품구분 == product)
+        
+    }
+    
+    if (gender != "all") {
+      
+      df %<>% filter(성별 == gender)
+      
+    }
+    
+    if (location != "all") {
+      
+      df %<>% filter(구매지역 == location)
+      
+    }
+      
+    df %<>% group_by(구매시점) %>% summarise(n = n()) %>% arrange(구매시점) 
+    
+    # plotting 
+    ggplot(df, aes(x = 구매시점, y = n)) +
+      geom_point() 
+    
+  }
+
+plotter(df = merged_potato, product = "상품B", gender = 2, location = "부산")
+
+
+merged_potato %>% filter(구매시점 == 1 & 상품구분 == "상품B") %>% group_by(구매지역) %>% 
+  summarise( range(평균기온) )
+
+nls()
